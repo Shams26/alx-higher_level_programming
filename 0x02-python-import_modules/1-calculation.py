bc76a7d9c0d9f421137827ce47863a0f09d76a9f
Shams26 <shamsudeenola6@gmail.com>
2023-08-11 19:48:05 -0700

    one

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+from calculator_1 import add
+a = 10
+b = 5
+
+print("{} + {} = {}".format(a, b, add(a, b)))
+
+from calculator_1 import sub
+print("{} - {} = {}".format(a, b, sub(a, b)))
+
+from calculator_1 import mul
+print("{} * {} = {}".format(a, b, mul(a, b)))
+
+from calculator_1 import div
+print("{} / {} = {}".format(a, b, div(a, b)))
