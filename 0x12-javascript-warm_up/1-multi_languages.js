c16d1af88218a5389b3938439ba1078c944331f3
Shams26 <shamsudeenola6@gmail.com>
2023-11-13 16:43:54 -0800

    FirstTwo

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const FirstLine = "C is fun"
+const SecondLine = "Python is cool"
+const ThirdLine = "JavaScript is amazine"
+
+console.log(FirstLine);
+console.log(SecondtLine);
+console.log(ThirdLine);
---
+++ 0x12-javascript-warm_up/README.md
@@ -0,0 +1,1 @@
+This folder holds my work on JavaScript warm-up
